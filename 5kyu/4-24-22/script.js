b25128c21177f9edd071b5da99dd607a4c625001
Aidan Williams <aidanw4@gmail.com>
2022-04-24 13:52:33 +0700

    complete daily kata challenge

---
+++ 5kyu/4-24-22/script.js
@@ -0,0 +1,19 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(arr) {
+  let result = [];
+  let zeroCount = 0;
+  arr.forEach((el) => {
+    if (el !== 0) {
+      result.push(el);
+    } else {
+      zeroCount++;
+    }
+  });
+  for (let i = 0; i < zeroCount; i++) {
+    result.push(0);
+  }
+  return result;
+}
