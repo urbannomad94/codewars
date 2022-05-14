62788d7782795e3db64b4e38f02265bdd6791b8d
Aidan Williams <aidanw4@gmail.com>
2022-05-14 08:28:22 +0700

    complete daily kata challenge

---
+++ 8kyu/5-26-22/script.js
@@ -0,0 +1,18 @@
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell ).
+
+function sumOfDifferences(arr) {
+  let sum = 0;
+  let sorted = arr.sort((a, b) => b - a);
+  for (let i = 0; i < sorted.length - 1; i++) {
+    sum += sorted[i] - sorted[i + 1];
+  }
+  return sum;
+}
