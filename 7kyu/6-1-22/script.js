53f6deb2b16f57de535d9076b597633ed5a757a6
Aidan Williams <aidanw4@gmail.com>
2022-06-01 16:48:19 +0700

    complete daily kata challenge

---
+++ 7kyu/6-1-22/script.js
@@ -0,0 +1,11 @@
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
