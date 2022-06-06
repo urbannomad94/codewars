fae803bbf8881ef360ecec1ea64d0670efca8c8a
Aidan Williams <aidanw4@gmail.com>
2022-06-06 13:22:25 +0700

    complete daily kata challenge

---
+++ 6kyu/3-22-22/script.js
@@ -0,0 +1,18 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// complete the function
+function solution(string) {
+    //split string into array where upper/lowercase changes
+  let arr = string.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[])
+    }
+  }
+}
