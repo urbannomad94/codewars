00aad6d4d77081c003ddfaac533d1ba35daa694a
Aidan Williams <aidanw4@gmail.com>
2022-11-02 09:30:26 +0700

    complete daily kata challenge

---
+++ 8kyu/11-2-22/script.js
@@ -0,0 +1,21 @@
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+function first(arr, n = 1) {
+  let result = [];
+  if (n > arr.length) {
+    return arr;
+  }
+  for (let i = 0; i < n; i++) {
+    result.push(arr[i]);
+  }
+  return result;
+}
